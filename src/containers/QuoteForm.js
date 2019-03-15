3032db13bc0fe5fa695750d44a4e182a833b384a
Jonathan Tuzman <tuzmusic@gmail.com>
2019-03-15 17:09:00 -0400

    stub redux in quoteform, functions in quote

--- src/containers/QuoteForm.js
+++ src/containers/QuoteForm.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react';
-import uuid from 'uuid';
-import { connect } from 'react-redux';
-import { addQuote } from '../actions/quotes';
+import React, { Component } from "react";
+import uuid from "uuid";
+import { connect } from "react-redux";
+import { addQuote } from "../actions/quotes";
 
 class QuoteForm extends Component {
-
   state = {
-    //set up a controlled form with internal state
-  }
+    id: "",
+    content: "",
+    author: ""
+  };
 
   handleOnChange = event => {
-    // Handle Updating Component State
-  }
+    // this.setState({...this.state, event.target.htmlFor: event.target.value})
+  };
 
   handleOnSubmit = event => {
     // Handle Form Submit event default
     // Create quote object from state
     // Pass quote object to action creator
     // Update component state to return to default state
-  }
+  };
 
   render() {
     return (
@@ -29,7 +30,9 @@ class QuoteForm extends Component {
               <div className="panel-body">
                 <form className="form-horizontal">
                   <div className="form-group">
-                    <label htmlFor="content" className="col-md-4 control-label">Quote</label>
+                    <label htmlFor="content" className="col-md-4 control-label">
+                      Quote
+                    </label>
                     <div className="col-md-5">
                       <textarea
                         className="form-control"
@@ -38,7 +41,9 @@ class QuoteForm extends Component {
                     </div>
                   </div>
                   <div className="form-group">
-                    <label htmlFor="author" className="col-md-4 control-label">Author</label>
+                    <label htmlFor="author" className="col-md-4 control-label">
+                      Author
+                    </label>
                     <div className="col-md-5">
                       <input
                         className="form-control"
@@ -49,7 +54,9 @@ class QuoteForm extends Component {
                   </div>
                   <div className="form-group">
                     <div className="col-md-6 col-md-offset-4">
-                      <button type="submit" className="btn btn-default">Add</button>
+                      <button type="submit" className="btn btn-default">
+                        Add
+                      </button>
                     </div>
                   </div>
                 </form>
@@ -61,6 +68,11 @@ class QuoteForm extends Component {
     );
   }
 }
-
+const mapStateToProps = state => {
+  return;
+};
+const mapDispatchToProps = dispatch => {
+  return;
+};
 //add arguments to connect as needed
-export default connect()(QuoteForm);
+export default connect(mapStateToProps, mapDispatchToProps)(QuoteForm);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,7 +1,35 @@
 export default (state = [], action) => {
   const quotes = [...state.quotes]; // CLONE of the state's quotes, allowed to mutate.
   const index = quotes.findIndex(q => q === action.quote); // find the action quote in our clone
-  const quote = quotes[index] // actual REFERENCE to the EXISTING quote in the CLONED array
+  const quote = quotes[index]; // actual REFERENCE to the EXISTING quote in the CLONED array
+
+  // function ADD_QUOTE() {
+  //   return [...this.quotes, this.action.quote];
+  // }
+  // function REMOVE_QUOTE() {
+  //   return [...this.quotes.slice(0, index), ...this.quotes.slice(index + 1)];
+  // }
+  // function UPVOTE_QUOTE() {
+  //   this.quotes[this.index].votes += 1; // allowed to mutate this clone
+  //   return this.quotes;
+  // }
+  // function DOWNVOTE_QUOTE() {
+  //   if (this.quote.votes > 0) this.quote.votes -= 1; // allowed to mutate this clone
+  //   return this.quotes;
+  // }
+
+  // const snakeToCamel = (str) => {
+  //   if (typeof(str) !== "string") return
+  //   const lower = str.toLowerCase()
+  //   const parts = lower.split('_')
+  //   const humps = parts.splice(1).map(str => {
+  //     str[0] = str[0].toUpperCase()
+  //     return str
+  //   })
+  //   return [parts[0], ...humps].join('')
+  // }
+
+  // return this[action.type] ? { ...state, quotes: this[action.type]() } : state;
 
   switch (action.type) {
     case "ADD_QUOTE":
@@ -14,8 +42,7 @@ export default (state = [], action) => {
       quote.votes += 1; // allowed to mutate this clone
       return { ...state, quotes: quotes };
     case "DOWNVOTE_QUOTE":
-    if (quote.votes > 0) 
-      quote.votes -= 1; // allowed to mutate this clone
+      if (quote.votes > 0) quote.votes -= 1; // allowed to mutate this clone
       return { ...state, quotes: quotes };
     default:
       return state;
