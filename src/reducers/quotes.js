a653c03770c891ef09da8390be6f9d6a785a559c
tuzmusic <tuzmusic@gmail.com>
2019-03-15 09:36:15 -0400

    write reducer, beautifully refactored

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,23 @@
 export default (state = [], action) => {
-  return state;
-}
+  const quotes = [...state.quotes]; // allowed to mutate this clone
+  const index = quotes.findIndex(q => q === action.quote); // find the action quote in our clone
+  const quote = quotes[index] // actual reference to the EXISTING quote in the cloned array
+
+  switch (action.type) {
+    case "ADD_QUOTE":
+      quotes.push(action.quote);
+      return { ...state, quotes: quotes };
+    case "REMOVE_QUOTE":
+      quotes.splice(index, 1);
+      return { ...state, quotes: quotes };
+    case "UPVOTE_QUOTE":
+      quote.votes += 1; // allowed to mutate this clone
+      return { ...state, quotes: quotes };
+    case "DOWNVOTE_QUOTE":
+    if (quote.votes > 0) 
+      quote.votes -= 1; // allowed to mutate this clone
+      return { ...state, quotes: quotes };
+    default:
+      return state;
+  }
+};
