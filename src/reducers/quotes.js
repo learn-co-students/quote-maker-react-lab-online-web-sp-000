cdc01a400224a66698cb77e83a8da92966c4ea46
kaelaclement <stephanie.kaela@gmail.com>
2019-06-27 13:30:24 -0700

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,31 @@
+import uuid from 'uuid';
+
+const id = uuid();
+
 export default (state = [], action) => {
-  return state;
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      const newQuote = { id: id, ...action.quote };
+      return [...state, newQuote];
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id != action.quoteId);
+    case 'UPVOTE_QUOTE':
+      const upvotedQuote = state.find(quote => quote.id == action.quoteId);
+      const preUpvoteState = state.filter(quote => quote.id != action.quoteId)
+      if (upvotedQuote.vote) {
+        return [...preUpvoteState, { ...upvotedQuote, votes: upvotedQuote.votes + 1 }]
+      } else {
+        return [...preUpvoteState, { ...upvotedQuote, votes: 1 }]
+      };
+    case 'DOWNVOTE_QUOTE':
+      const downvotedQuote = state.find(quote => quote.id == action.quoteId);
+      let preDownvoteState = state.filter(quote => quote.id != action.quoteId)
+      if (downvotedQuote.votes > 0) {
+        return [...preDownvoteState, { ...downvotedQuote, votes: downvotedQuote.votes - 1 }];
+      } else {
+        return [...preDownvoteState, { ...downvotedQuote, votes: 0 }];
+      }
+    default:
+      return state;
+  }
 }
