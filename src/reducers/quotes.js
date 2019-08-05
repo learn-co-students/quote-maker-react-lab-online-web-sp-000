11307a8757e29b34fdc8a811ce044641f79b242b
cdallasanta <chris.dallasanta@gmail.com>
2019-08-05 15:33:28 -0700

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,24 @@
 export default (state = [], action) => {
-  return state;
+  let index;
+
+  switch(action.type){
+    case "ADD_QUOTE":
+      return [...state, action.quote]
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id !== action.quoteId)
+    case "UPVOTE_QUOTE":
+      index = state.findIndex(q => q.id === action.quoteId)
+      let upvoted = state;
+      upvoted[index].votes++;
+      return upvoted;
+    case "DOWNVOTE_QUOTE":
+      index = state.findIndex(q => q.id === action.quoteId)
+      let downvoted = state;
+      if (downvoted[index].votes > 0){
+        downvoted[index].votes--;
+      }
+      return downvoted;
+    default:
+      return state
+  }
 }
