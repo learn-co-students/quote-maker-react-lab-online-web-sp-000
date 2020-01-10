6c20675a7d0b50d4dddac7c6fd624cfc931d5bde
Felipe <fb.dc.col@gmail.com>
2020-01-10 15:43:37 -0300

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,43 @@
 export default (state = [], action) => {
-  return state;
+  let index
+  let quote
+  switch(action.type){
+    case "ADD_QUOTE":
+      return [...state, action.quote];   
+
+    case "REMOVE_QUOTE":  
+      return state.filter(quote => quote.id !== action.quoteId);
+
+    case 'UPVOTE_QUOTE':
+      index = state.findIndex(q => action.quoteId === q.id);
+      quote = state[index];
+      return [
+        ...state.slice(0, index),
+        {
+          id: quote.id,
+          content: quote.content,
+          author: quote.author,
+          votes: quote.votes + 1
+        }
+      ];
+
+    case 'DOWNVOTE_QUOTE':
+      index = state.findIndex(q => action.quoteId === q.id);
+      quote = state[index];
+      if (quote.votes === 0) {
+        return state;
+      } else {
+        return [
+          ...state.slice(0, index),
+          {
+            id: quote.id,
+            content: quote.content,
+            author: quote.author,
+            votes: quote.votes - 1
+          }
+        ];
+      }
+
+    default: return state;
+  }  
 }
