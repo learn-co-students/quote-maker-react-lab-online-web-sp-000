5c2d955ab8c01659c8377b9c8b745ef4f83e32b0
tiffnuugen <tiffanynuugen@gmail.com>
2020-02-05 21:13:46 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,32 @@
 export default (state = [], action) => {
-  return state;
-}
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      // console.log('add quote:', action);
+      const quote = {
+        id: action.quote.id,
+        content: action.quote.content,
+        author: action.quote.author,
+        votes: 0
+      };
+      return [...state, quote];
+    case 'REMOVE_QUOTE':
+      // console.log('remove quote:', action);
+      // console.log('new state', state);
+      return state.filter((quote) => quote.id !== action.quoteId);
+    case 'UPVOTE_QUOTE':
+      console.log('upvote quote:', action);
+      console.log('new state', state);
+      return state.map((quote) =>
+        quote.id === action.quoteId ? { ...quote, votes: ++quote.votes } : quote
+      );
+    case 'DOWNVOTE_QUOTE':
+      // console.log('downvote quote:', action);
+      return state.map((quote) =>
+        quote.id === action.quoteId && quote.votes !== 0
+          ? { ...quote, votes: --quote.votes }
+          : quote
+      );
+    default:
+      return state;
+  }
+};
