a0924fc7cfd7a004dcb24c811880b2ad06561a52
boom100100 <bsd231@nyu.edu>
2020-03-07 14:22:24 -0500

    Done.

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -5,19 +5,26 @@ import { addQuote } from '../actions/quotes';
 
 class QuoteForm extends Component {
 
-  state = {
-    //set up a controlled form with internal state
+  constructor(){
+    super();
+    this.state = { content: '', author: '' }
   }
 
   handleOnChange = event => {
-    // Handle Updating Component State
+    this.setState({
+      [event.target.name]: event.target.value,
+    });// Handle Updating Component State
   }
 
   handleOnSubmit = event => {
-    // Handle Form Submit event default
+    event.preventDefault();
+
     // Create quote object from state
     // Pass quote object to action creator
+    this.props.addQuote(this.state);
+
     // Update component state to return to default state
+    this.setState({ content: '', author: '' });
   }
 
   render() {
@@ -27,12 +34,12 @@ class QuoteForm extends Component {
           <div className="col-md-8 col-md-offset-2">
             <div className="panel panel-default">
               <div className="panel-body">
-                <form className="form-horizontal">
+                <form className="form-horizontal" onSubmit={this.handleOnSubmit}>
                   <div className="form-group">
                     <label htmlFor="content" className="col-md-4 control-label">Quote</label>
                     <div className="col-md-5">
                       <textarea
-                        className="form-control"
+                        className="form-control" name="content" onChange={this.handleOnChange}
                         value={this.state.content}
                       />
                     </div>
@@ -42,7 +49,7 @@ class QuoteForm extends Component {
                     <div className="col-md-5">
                       <input
                         className="form-control"
-                        type="text"
+                        type="text" name="author" onChange={this.handleOnChange}
                         value={this.state.author}
                       />
                     </div>
@@ -62,5 +69,9 @@ class QuoteForm extends Component {
   }
 }
 
+const mapDispatchToProps = dispatch => ({
+  addQuote: formData => dispatch({ type: 'ADD_QUOTE', quote: formData })
+})
+
 //add arguments to connect as needed
-export default connect()(QuoteForm);
+export default connect(null, mapDispatchToProps)(QuoteForm);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,59 @@
+import '../actions/quotes'
+
 export default (state = [], action) => {
-  return state;
+  let index;
+
+  switch (action.type) {
+
+    case 'ADD_QUOTE':
+      state.push(action.quote);
+      return state;
+
+    case 'REMOVE_QUOTE':
+    index = state.indexOf(
+      state.map(
+        element => {if (element.id === action.quoteId)
+          return element
+        }
+      )[0]
+    );
+      state.splice(index,1);
+      return state;
+
+    case 'UPVOTE_QUOTE':
+      index = state.indexOf(
+        state.map(
+          element => {if (element.id === action.quoteId)
+            return element
+          }
+        )[0]
+      );
+
+      if (!state[index].votes){
+        state[index].votes = 0;
+      }
+      state[index].votes++;
+      return state;
+
+    case 'DOWNVOTE_QUOTE':
+      index = state.indexOf(
+        state.map(
+          element => {if (element.id === action.quoteId)
+            return element
+          }
+        )[0]
+      );
+
+      if (!state[index].votes){
+        state[index].votes = 0;
+      }
+      if (state[index].votes !== 0){
+        state[index].votes--;
+      }
+
+      return state;
+
+    default:
+      return state;
+  }
 }
