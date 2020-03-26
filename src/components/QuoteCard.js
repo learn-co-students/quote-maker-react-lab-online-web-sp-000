571a35654b9c04a85c73f7957c879e08b8c81956
ch2isk4kos <ch2isk4kos@gmail.com>
2020-03-26 12:47:05 -0400

    Done.

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,38 +1,32 @@
 import React from 'react';
 
-const QuoteCard = (props) =>
-  <div>
-    <div className="card card-inverse card-success card-primary mb-3 text-center">
-      <div className="card-block">
-        <blockquote className="card-blockquote">
-          {/* <p>{Render Quote Content}</p> */}
-          {/* <footer>- author <cite title="Source Title">{Render Quote Author}</cite></footer> */}
-        </blockquote>
-      </div>
-      <div className="float-right">
-        <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
-          <button
-            type="button"
-            className="btn btn-primary"
-          >
-            Upvote
-          </button>
-          <button
-            type="button"
-            className="btn btn-secondary"
-          >
-            Downvote
-          </button>
-          <button
-            type="button"
-            className="btn btn-danger"
-          >
-            <span aria-hidden="true">&times;</span>
-          </button>
+// const QuoteCard = (props) =>
+const QuoteCard = ({ quote, removeQuote, upvoteQuote, downvoteQuote }) =>
+    <div>
+        <div className="card card-inverse card-success card-primary mb-3 text-center">
+            <div className="card-block">
+                <blockquote className="card-blockquote">
+                    {/* <p>{Render Quote Content}</p> */}
+                    <p>{quote.content}</p>
+                    {/* <footer>- author <cite title="Source Title">{Render Quote Author}</cite></footer> */}
+                    <footer>- author <cite title="Source Title">{quote.author}</cite></footer>
+                </blockquote>
+            </div>
+            <div className="float-right">
+                <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
+                    {/* Upvote Quote */}
+                    <button type="button" className="btn btn-primary" onClick={() => upvoteQuote(quote.id)}>Upvote</button>
+                    {/* Downvote Quote */}
+                    <button type="button" className="btn btn-secondary" onClick={() => downvoteQuote(quote.id)}>Downvote</button>
+                    {/* Remove Quote */}
+                    <button type="button" className="btn btn-danger" onClick={() => removeQuote(quote.id)}>
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                {/* <div>Votes: {Render Quote Votes}</div> */}
+                <div>{ quote.votes }</div>
+            </div>
         </div>
-        {/* <div>Votes: {Render Quote Votes}</div> */}
-      </div>
-    </div>
-  </div>;
+    </div>;
 
 export default QuoteCard;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
 import { Provider } from 'react-redux';
 import { createStore } from 'redux'
+
 import rootReducer from './reducers/index'
 
 let store = createStore(rootReducer)
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App store={store}/>
+    </Provider>,
+    document.getElementById('root')
 );
