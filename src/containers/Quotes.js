b5950dd173c5c0015da2527516743256b9704807
jeremyamram <52257264+jeremyamram@users.noreply.github.com>
2020-04-19 13:35:07 +0100

    Done.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
+import { removeQuote,upvoteQuote, downvoteQuote } from '../actions/quotes';
 
 class Quotes extends Component {
 
   render() {
+    const { quotes, removeQuote, upvoteQuote, downvoteQuote } = this.props;
+    console.log(this.props);
     return (
       <div>
         <hr />
@@ -20,6 +23,14 @@ class Quotes extends Component {
 
                 Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
                */}
+               { 
+                quotes.map(quote => <QuoteCard 
+                    quote={quote} 
+                    removeQuote={removeQuote} 
+                    upvoteQuote={upvoteQuote} 
+                    downvoteQuote={downvoteQuote}
+                  />)
+                }
             </div>
           </div>
         </div>
@@ -28,5 +39,19 @@ class Quotes extends Component {
   }
 }
 
-//add arguments to connect as needed
-export default connect()(Quotes);
+const mapStateToProps = ({ quotes }) => {
+  return {
+    quotes
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    removeQuote: (id) => dispatch(removeQuote(id)),
+    upvoteQuote: (id) => dispatch(upvoteQuote(id)),
+    downvoteQuote: (id) => dispatch(downvoteQuote(id))
+
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Quotes);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,28 @@
 export default (state = [], action) => {
+
+  const quoteIndex = state.findIndex(i => i.id === action.quoteId)
+  const copiedState = [ ...state ]
+
+  switch(action.type){
+    case'ADD_QUOTE': 
+      return [ ...state, action.quote ] 
+    case'REMOVE_QUOTE': 
+      return [ ...state ].filter(item=>item.id !== action.quoteId)
+
+    case'UPVOTE_QUOTE': 
+      copiedState[quoteIndex].votes += 1
+      return copiedState
+
+    case'DOWNVOTE_QUOTE': 
+      if (copiedState[quoteIndex].votes>0) {
+        copiedState[quoteIndex].votes-=1
+        return copiedState
+      }
+      break;
+
+    default:
+      return copiedState;
+      
+  }
   return state;
 }
