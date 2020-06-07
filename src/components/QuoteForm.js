34a55b351c59c8ab92abee2836c3a57a411ca26b
github username <github email address>
2020-06-06 23:04:45 -0400

    Done.

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -1,23 +1,38 @@
 import React, { Component } from 'react';
 import uuid from 'uuid';
 import { connect } from 'react-redux';
-import { addQuote } from '../actions/quotes';
+import { addQuote } from '../actions/quotes';  /////this makes addQuote a prop for QuoteForm .
 
 class QuoteForm extends Component {
 
+
   state = {
-    //set up a controlled form with internal state
+      //set up a controlled form with internal state
+    content: '',
+    author: ''
   }
+
 
   handleOnChange = event => {
     // Handle Updating Component State
+    const { value, name } = event.target;
+    this.setState({
+      [name]: value
+    });
   }
 
   handleOnSubmit = event => {
     // Handle Form Submit event default
+    event.preventDefault();
     // Create quote object from state
+    let quote = {...this.state, id: uuid() }; /////////// Understood.
     // Pass quote object to action creator
+    this.props.addQuote(quote);   //////////// This prop was imported above.
     // Update component state to return to default state
+    this.setState({
+      content: '',
+      author: ''
+    })
   }
 
   render() {
@@ -27,11 +42,13 @@ class QuoteForm extends Component {
           <div className="col-md-8 col-md-offset-2">
             <div className="panel panel-default">
               <div className="panel-body">
-                <form className="form-horizontal">
+                <form className="form-horizontal" onSubmit={this.handleOnSubmit}>
                   <div className="form-group">
                     <label htmlFor="content" className="col-md-4 control-label">Quote</label>
                     <div className="col-md-5">
                       <textarea
+                        onChange={this.handleOnChange}
+                        name="content"
                         className="form-control"
                         value={this.state.content}
                       />
@@ -41,6 +58,8 @@ class QuoteForm extends Component {
                     <label htmlFor="author" className="col-md-4 control-label">Author</label>
                     <div className="col-md-5">
                       <input
+                        onChange={this.handleOnChange}
+                        name="author"
                         className="form-control"
                         type="text"
                         value={this.state.author}
@@ -63,4 +82,4 @@ class QuoteForm extends Component {
 }
 
 //add arguments to connect as needed
-export default connect()(QuoteForm);
+export default connect(null, { addQuote })(QuoteForm); ////// I believe "null" is the mapToProps argument(?) and "{ addQuote }" is the mapToDispatch argument.
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -9,59 +9,31 @@ console.log(id);
 
 export default (state = [], action) => {
   let idx;
+  let newArray;
   switch (action.type) {
     case "ADD_QUOTE":
       return [...state, action.quote]
     case "REMOVE_QUOTE":
-      let newArray = state.slice();
-      return newArray.filter(quote => quote.id !== action.quoteId);
-    //case "REMOVE_QUOTE":
-      // let newArray = [...state];
-      // return newArray.filter(quote => quote.id !== action.id);
-      //return [...state.slice.];
-      // idx = state.findIndex(quote => quote.id === action.id);
-      // return [...state.slice(0, idx), ...state.slice(idx + 1)];
+      // let newArray = state.slice();
+      // return newArray.filter(quote => quote.id !== action.quoteId);
+
+      idx = state.findIndex(quote => quote.id === action.quoteId);
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
 
     case "UPVOTE_QUOTE":
-      return
+      newArray = state.slice();
+      idx = state.findIndex(quote => quote.id === action.quoteId);
+      newArray[idx].votes += 1;
+      return newArray;
     case "DOWNVOTE_QUOTE":
-      return
+      newArray = state.slice();
+      idx = state.findIndex(quote => quote.id === action.quoteId);
+      if (newArray[idx].votes > 0) {
+        newArray[idx].votes -= 1
+        return newArray
+      }
+      return newArray;
     default:
       return state;
   }
 }
-
-
-
-
-
-// export default function manageQuote(state = [], action) {
-//   switch (action.type) {
-//     case "ADD_QUOTE":
-//       console.log('here');
-//     case "REMOVE_QUOTE":
-//       return []
-//     case "UPVOTE_QUOTE":
-//       return
-//     case "DOWNVOTE_QUOTE":
-//       return
-//     default:
-//       return state;
-//   }
-// }
-
-
-
-
-
-       // addQuote(quote: {})
-       //   should return an Object with a type of "ADD_QUOTE" and a quote object:
-       //
-       // removeQuote(quoteId: Integer)
-       //   should return an Object with a type of "REMOVE_QUOTE" and a quoteId:
-       //
-       // upvoteQuote(quoteId: Integer)
-       //   should return an Object with a type of "UPVOTE_QUOTE" and a quoteId:
-       //
-       // downvoteQuote(quoteId: INteger)
-       //   should return an Object with a type of "DOWNVOTE_QUOTE" and a quoteId:
