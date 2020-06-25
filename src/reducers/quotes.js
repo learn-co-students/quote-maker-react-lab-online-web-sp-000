9a4875d298a9dcfa3173530c0659acc34963e9e3
jkellyphilly <joel.kelly.philly@gmail.com>
2020-06-25 15:29:32 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,20 @@
 export default (state = [], action) => {
-  return state;
+  let idx;
+  switch(action.type) {
+    case 'ADD_QUOTE':
+      return [...state, action.quote];
+
+    case 'REMOVE_QUOTE':
+      idx = state.findIndex(quote => quote.id === action.id)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+
+    case 'UPVOTE_QUOTE':
+      return 1;
+
+    case 'DOWNVOTE_QUOTE':
+      return 1;
+
+    default:
+      return state;
+  }
 }
