b0e937cce405772f949126b5b879829da76d4165
Malcolm_Leake <echelon1089@gmail.com>
2020-06-25 14:59:17 -0400

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,32 @@
 // TODO: Create action creators as defined in tests
+
+import { object } from "chai-spies"
+
+
+export const addQuote = (quote) => {
+    return {
+        type: 'ADD_QUOTE',
+        quote: Object.assign({}, quote, {votes: 0})
+    }
+}
+
+export const removeQuote = (quote) => {
+    return {
+        type: 'REMOVE_QUOTE',
+        quoteId: quote
+    }
+}
+
+export const upvoteQuote = (quote) => {
+    return {
+        type: 'UPVOTE_QUOTE',
+        quoteId: quote
+    }
+}
+
+export const downvoteQuote = (quote) => {
+    return {
+        type: 'DOWNVOTE_QUOTE',
+        quoteId: quote
+    }
+}
--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
-const QuoteCard = (props) =>
+const QuoteCard = (props) =>{
+console.log(props)
+return(
   <div>
+   
+     
     <div className="card card-inverse card-success card-primary mb-3 text-center">
       <div className="card-block">
         <blockquote className="card-blockquote">
-          {/* <p>{Render Quote Content}</p> */}
-          {/* <footer>- author <cite title="Source Title">{Render Quote Author}</cite></footer> */}
+          {props.quote.content}
+          {props.quote.author}
+          {props.quote.votes}
         </blockquote>
       </div>
       <div className="float-right">
@@ -33,6 +38,7 @@ const QuoteCard = (props) =>
         {/* <div>Votes: {Render Quote Votes}</div> */}
       </div>
     </div>
-  </div>;
+  </div>)
+}
 
 export default QuoteCard;
