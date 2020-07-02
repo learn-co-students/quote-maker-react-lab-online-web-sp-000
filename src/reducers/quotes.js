f93c4b35c7c54dbbe0e652cd23ac151dd1a4a248
tcrurup <tcrurup@gmail.com>
2020-07-02 14:51:45 -0600

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,30 @@
 export default (state = [], action) => {
-  return state;
+  
+  const alterVotes = (quoteId, amount) => {
+    let newObj = [...state]
+    console.log(newObj)
+    const index = newObj.findIndex( quote => quote.id === action.quoteId )
+    
+    newObj[index].votes += amount;
+    if(newObj[index].votes < 0){
+      newObj[index].votes = 0
+    }
+    
+    return newObj 
+  }
+
+  switch(action.type){    
+    case "ADD_QUOTE":
+      return [...state, action.quote] 
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id != action.quoteId)
+    case "UPVOTE_QUOTE":
+      return alterVotes(action.quoteId, 1)
+    case "DOWNVOTE_QUOTE":
+      return alterVotes(action.quoteId, -1)
+    default:
+      return state;
+  }
 }
+
+
