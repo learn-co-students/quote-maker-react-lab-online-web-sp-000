7100f552420b60cee1400004beb4b3cef33929b7
rehmanali001 <rehmanali001@gmail.com>
2020-07-14 15:59:44 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,60 @@
+import uuid from 'uuid';
+
 export default (state = [], action) => {
-  return state;
-}
+  let id;
+  let quoteToUpdate;
+  let index;
+  let votes;
+
+  if(action.type !== 'ADD_QUOTE'){
+    id = action.quoteId;
+    index = state.findIndex(quote => quote.id === id);
+
+    quoteToUpdate = state[index];
+    console.log(state)
+    console.log(id)
+    console.log(index)
+    console.log(quoteToUpdate)
+    if(quoteToUpdate == undefined){
+     return state; 
+    }else{
+      votes = quoteToUpdate.votes;
+    }
+  }
+
+  switch(action.type){
+    case 'ADD_QUOTE':
+      return state.concat(action.quote)
+
+    case 'REMOVE_QUOTE':
+      return [...state.slice(0, index), ...state.slice(index + 1)]
+
+    case 'UPVOTE_QUOTE':
+      console.log(quoteToUpdate)
+      return [
+        ...state.slice(0, index),
+        {
+          ...quoteToUpdate,
+          votes: quoteToUpdate.votes + 1
+        }, 
+        ...state.slice(index + 1)
+      ]
+
+    case 'DOWNVOTE_QUOTE':
+      if(votes > 0){
+        return [
+          ...state.slice(0, index),
+          {
+            ...quoteToUpdate,
+            votes: votes - 1
+          }, 
+          ...state.slice(index + 1)
+        ]
+      }else{
+        return state;
+      }
+
+    default:
+      return state;
+  }
+}	
