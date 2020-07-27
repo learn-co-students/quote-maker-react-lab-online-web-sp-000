a926e9527f9ba5675dc1a6ea237e5c87725b216f
nathan-stamand <nathansaint810@gmail.com>
2020-07-27 13:51:36 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,22 @@
 export default (state = [], action) => {
-  return state;
+  let idx
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return [...state, action.quote]
+    case 'REMOVE_QUOTE':
+      idx = state.findIndex(quote => quote.id === action.quoteId)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)]
+    case 'UPVOTE_QUOTE':
+      idx = state.findIndex(quote => quote.id === action.quoteId)
+      let quote = state[idx]
+      quote.votes += 1
+      return [...state.slice(0, idx), quote, ...state.slice(idx + 1)]
+    case 'DOWNVOTE_QUOTE':
+      idx = state.findIndex(quote => quote.id === action.quoteId)
+      quote = state[idx]
+      quote.votes > 0 ? quote.votes -= 1 : null;
+      return [...state.slice(0, idx), quote, ...state.slice(idx + 1)]
+    default:
+      return state;
+  }
 }
