d2b4fc41a9e897dfd5d9dcf058b166ae33dc3691
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-09-10 16:21:33 +0200

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,37 +1,21 @@
-export default (state = [], action ) => {
-
-  let newState;
-
-  switch(action.type) {
-    case 'ADD_QUOTE':
-      return [...state, action.quote]
-
-    case 'REMOVE_QUOTE':
-      return state.filter(quote => quote.id !== action.quoteId)
-
-    case 'UPVOTE_QUOTE':
-      newState = state.map(quote => {
-        if (quote.id === action.quoteId) {
-          return {...quote, votes: ++quote.votes}
-        } else {
-          return quote
-        }
-      })
-      return newState;
-
-      case 'DOWNVOTE_QUOTE':
-        newState = state.map(quote => {
-          if (quote.id === action.quoteId && quote.votes > 0) {
-            return {...quote, votes: --quote.votes}
-          } else {
-            return quote
-          }
-        })
-      return newState;
-
-    default: 
+export default (state = [], action) => {
+  let idx;
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote];
+    case "REMOVE_QUOTE":
+      idx = state.findIndex(quote => quote.id === action.quoteId);
+      return [...state.slice(0, idx), ...state.slice(idx+1)];
+    case "UPVOTE_QUOTE":
+      state.find(quote => quote.id === action.quoteId).votes += 1;
+      return state;
+    case "DOWNVOTE_QUOTE":
+      let quoteToDownvote = state.find(quote => quote.id === action.quoteId);
+      if (quoteToDownvote.votes > 0) {
+        quoteToDownvote.votes -= 1;
+      }
+      return state;
+    default:
       return state;
   }
-
-
 }
