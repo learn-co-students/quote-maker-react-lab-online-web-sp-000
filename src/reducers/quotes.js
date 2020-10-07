fec1173bedf0164f9779db075c560a58956550f3
wkdewey <wkdewey@gmail.com>
2020-10-07 13:50:48 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,39 @@
-export default (state = [], action) => {
-  return state;
+export default function quote(state = [], action) {
+  switch (action.type) {
+    case "ADD_QUOTE":
+      const quote = {
+        id: action.quote.id,
+        content: action.quote.content,
+        author: action.quote.author,
+        votes: action.quote.votes,
+      };
+      return state.concat(quote);
+    case "REMOVE_QUOTE":
+      return state.filter((quote) => quote.id !== action.quoteId);
+    case "UPVOTE_QUOTE":
+      let quoteIndex = state.findIndex((quote) => quote.id === action.quoteId);
+      let newQuotes = [...state];
+      console.log(newQuotes);
+      newQuotes[quoteIndex] = {
+        ...newQuotes[quoteIndex],
+        votes: newQuotes[quoteIndex].votes + 1,
+      };
+      console.log(newQuotes);
+      return newQuotes;
+    case "DOWNVOTE_QUOTE":
+      quoteIndex = state.findIndex((quote) => quote.id === action.quoteId);
+      if (state[quoteIndex].votes > 0) {
+        newQuotes = [...state];
+        newQuotes[quoteIndex] = {
+          ...newQuotes[quoteIndex],
+          votes: newQuotes[quoteIndex].votes - 1,
+        };
+        return newQuotes;
+      } else {
+        return state;
+      }
+
+    default:
+      return state;
+  }
 }
