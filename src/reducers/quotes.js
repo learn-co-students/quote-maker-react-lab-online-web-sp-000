14817e0807061d3b4d3f11ae1d337290ca5627aa
agiletkiewicz <a.letkiewicz@gmail.com>
2020-10-15 12:34:08 -0700

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,22 @@
 export default (state = [], action) => {
-  return state;
+  switch (action.type) {
+  case "ADD_QUOTE":
+    return [...state, action.quote]
+  case "REMOVE_QUOTE":
+    const removeMe = state.findIndex( quote => quote.id === action.Id );
+    const newArr = [...state];
+    return newArr.splice(removeMe, 1)
+  case "UPVOTE_QUOTE":
+    return state.map( quote => quote.id === action.quoteId ?
+      {...quote, votes: quote.votes + 1} :
+      quote
+      )
+    case "DOWNVOTE_QUOTE":
+      return state.map( quote => quote.id === action.quoteId ?
+        ( quote.votes > 0 ? {...quote, votes: quote.votes - 1} : quote ) :
+        quote
+        )
+  default:
+    return state;
+  }
 }
