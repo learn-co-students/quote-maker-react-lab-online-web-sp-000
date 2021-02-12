fc7a300932ea0a13c4da50fbfc66401f46d40948
cantstopcoding <jonathan_koss@yahoo.com>
2021-02-12 11:41:10 -0500

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,28 @@
 // TODO: Create action creators as defined in tests
+export const addQuote = quote => {
+    return {
+        quote: Object.assign({}, quote, {votes: 0}),
+        type: 'ADD_QUOTE'
+    }
+}
+
+export const removeQuote = quoteId => {
+    return {
+        type: 'REMOVE_QUOTE',
+        quoteId: quoteId
+    }
+}
+
+export const upvoteQuote = quoteId => {
+    return {
+        type: 'UPVOTE_QUOTE',
+        quoteId
+    }
+}
+
+export const downvoteQuote = quoteId => {
+    return {
+        type: 'DOWNVOTE_QUOTE',
+        quoteId
+    }
+}
