2832742356fdd9a290288a749086d9f390dcaba9
ismayton <ian.mayton@gmail.com>
2021-03-18 19:01:26 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,21 @@
 export default (state = [], action) => {
-  return state;
+  switch(action.type) {
+    
+    case "ADD_QUOTE":
+      return [ ...state,
+        {author: action.quote.author,
+        content: action.quote.content,
+        id: action.quote.id,
+        votes: action.quote.votes}
+      ]
+
+      case "REMOVE_QUOTE":
+        let idx = state.findIndex(quote => quote.id === action.quoteId);
+        console.log("Index: " + idx)
+        return [...state.slice(0, idx), ...state.slice(idx + 1)];
+     
+
+    default: 
+      return state
+  }
 }
