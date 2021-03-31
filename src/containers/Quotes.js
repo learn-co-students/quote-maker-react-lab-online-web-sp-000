6caa1fc17ee4dcf6d0177f2963869ddbc0f19de7
john million <jpmillion@comcast.net>
2021-03-30 21:00:07 -0400

    Done.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
+import { upvoteQuote, downvoteQuote, removeQuote } from '../actions/quotes';
 
 class Quotes extends Component {
 
+  handleUpvote = id => this.props.upvoteQuote(id)
+
+  renderQuotes = () => {
+    return this.props.quotes.map(q => (
+      <QuoteCard key={q.id} quote={q} upvoteQuote={() => this.props.upvoteQuote(q.id)} downvoteQuote={() => this.props.downvoteQuote(q.id)} removeQuote={() => this.props.removeQuote(q.id)} />
+    ))
+  }
+  
+
   render() {
+    console.log(this.props.quotes)
     return (
       <div>
         <hr />
@@ -20,6 +31,7 @@ class Quotes extends Component {
 
                 Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
                */}
+               {this.renderQuotes()}
             </div>
           </div>
         </div>
@@ -28,5 +40,6 @@ class Quotes extends Component {
   }
 }
 
+
 //add arguments to connect as needed
-export default connect()(Quotes);
+export default connect(state => state, {upvoteQuote, downvoteQuote, removeQuote})(Quotes);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,31 @@
-export default (state = [], action) => {
-  return state;
+
+const quotes = (state = [], action) => {
+  let idx;
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return [...state, action.quote]
+
+    case 'REMOVE_QUOTE':
+      return state.filter(q => q.id !== action.quoteId);
+
+    case 'UPVOTE_QUOTE':
+      console.log(action.quoteId)
+      let qu = state.find(q => q.id === action.quoteId);
+      qu.votes++;
+      let rest = state.filter(q => q.id !== action.quoteId);
+      return [...rest, qu]
+
+    case 'DOWNVOTE_QUOTE':
+      const quote = state.filter(q => q.id === action.quoteId);
+      const restOfState = state.filter(q => q.id !== action.quoteId);
+      if (quote[0].votes) {
+        quote[0].votes--;
+      }
+      return [...restOfState, ...quote]
+
+    default:
+      return state;
+  }
 }
+
+export default quotes;
