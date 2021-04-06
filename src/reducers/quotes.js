4c55030269b0b04d087e1272ae01f202669b9a71
ArslanInfinite <ali.arslan319@gmail.com>
2021-04-05 22:50:30 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,21 @@
+import uuid from 'uuid';
+
 export default (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      const quote = {
+        id: uuid(), 
+        content: action.content, 
+        author: action.author, 
+        votes: action.votes
+      }
+      return { state: state.quote.concat(quote)}
+    case 'REMOVE_QUOTE':
+      return { state: state.filter(quote => quote.id !== action.id)}
+    case 'UPVOTE_QUOTE':
+      return { state: state.quote.id + 1}
+    case 'DOWNVOTE_QUOTE':
+      return { state: state.quote.id - 1 }
+  }
   return state;
 }
