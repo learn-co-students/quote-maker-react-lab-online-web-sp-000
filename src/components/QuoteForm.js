555f2de815143c0b99fc1935eae542673f1fb23b
Chandra Lama <iamme24cl@gmail.com>
2021-04-26 02:51:43 -0700

    Done.

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -7,10 +7,16 @@ class QuoteForm extends Component {
 
   state = {
     //set up a controlled form with internal state
+    content: '',
+    author: ''
   }
 
   handleOnChange = event => {
     // Handle Updating Component State
+    const { value, name } = event.target;
+    this.setState({
+      [name]: value
+    })
   }
 
   handleOnSubmit = event => {
@@ -18,6 +24,13 @@ class QuoteForm extends Component {
     // Create quote object from state
     // Pass quote object to action creator
     // Update component state to return to default state
+    event.preventDefault();
+    const quote = {...this.state, id: uuid()};
+    this.props.addQuote(quote);
+    this.setState({
+      content: '',
+      author: ''
+    })
   }
 
   render() {
