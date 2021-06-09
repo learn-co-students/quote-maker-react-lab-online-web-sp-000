9e84cfbda38dc5d7c6b6bbe5efda2cb2b8f6c59b
A.J. Romaniello <ajiellodev@gmail.com>
2021-06-09 10:46:44 -0600

    Done.

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,38 +1,50 @@
 import React from 'react';
 
-const QuoteCard = (props) =>
-  <div>
-    <div className="card card-inverse card-success card-primary mb-3 text-center">
-      <div className="card-block">
-        <blockquote className="card-blockquote">
-          {/* <p>{Render Quote Content}</p> */}
-          {/* <footer>- author <cite title="Source Title">{Render Quote Author}</cite></footer> */}
-        </blockquote>
-      </div>
-      <div className="float-right">
-        <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
-          <button
-            type="button"
-            className="btn btn-primary"
-          >
-            Upvote
-          </button>
-          <button
-            type="button"
-            className="btn btn-secondary"
-          >
-            Downvote
-          </button>
-          <button
-            type="button"
-            className="btn btn-danger"
-          >
-            <span aria-hidden="true">&times;</span>
-          </button>
+const QuoteCard = (props) => {
+  const getVotes = () => {
+    return props.quote.votes
+  }
+
+  return (
+      <div>
+        <div className="card card-inverse card-success card-primary mb-3 text-center">
+          <div className="card-block">
+            <blockquote className="card-blockquote">
+              <p>{props.quote.content}</p>
+              <footer>- Author <cite title='Source Title'>{props.quote.author}</cite></footer>
+            </blockquote>
+          </div>
+          <div className="float-right">
+            <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
+              <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={() => props.upvoteQuote(props.quote.quoteId)}
+              >
+                Upvote
+              </button>
+              <button
+                  type="button"
+                  className="btn btn-secondary"
+                  onClick={() => props.downvoteQuote(props.quote.quoteId)}
+              >
+                Downvote
+              </button>
+              <button
+                  type="button"
+                  className="btn btn-danger"
+                  onClick={ () => props.removeQuote(props.quote.quoteId)}
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div>Votes: {getVotes()}</div>
+          </div>
         </div>
-        {/* <div>Votes: {Render Quote Votes}</div> */}
       </div>
-    </div>
-  </div>;
+  )
+
+}
+
 
 export default QuoteCard;
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,24 @@
 export default (state = [], action) => {
-  return state;
+  // console.log("Current State", state, 'Action', action)
+  switch(action.type) {
+    case 'ADD_QUOTE':
+      return [...state, action.quote]
+    case 'REMOVE_QUOTE':
+      const xState = [...state]
+      const l = xState.findIndex(e => e.quoteId === action.id)
+      xState.splice(l, 1)
+      return xState
+    case 'UPVOTE_QUOTE':
+      const i = state.findIndex(e => e.quoteId === action.id)
+      const newState = [...state]
+      newState[i].votes += 1
+      return newState
+    case 'DOWNVOTE_QUOTE':
+      const idx = state.findIndex(e => e.quoteId === action.id)
+      if (state[idx].votes <= 0) return state
+      const nState = [...state]
+      nState[idx].votes -= 1
+      return nState
+    default: return state;
+  }
 }
