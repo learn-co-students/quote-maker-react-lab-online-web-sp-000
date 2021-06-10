e25f45db07d8917fda691fa9f06ae2f0d916373e
deliaconstantino <dconstan92@gmail.com>
2021-06-10 11:24:56 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,9 @@
 export default (state = [], action) => {
+
+  //switch (action.type)
+  // case add, create uuid and add to array
+  // case remove
+  // case upvote
+  //case downvote
   return state;
 }
